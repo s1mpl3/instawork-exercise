e2f0be3a8ff93579595b5692a37f4c5082942938
Jorge <jiriso@gmail.com>
2025-03-17 13:19:52 -0700

    Add Loading message with a forced delay

--- app/src/app/teams/pageTeams.tsx
+++ app/src/app/teams/pageTeams.tsx
@@ -5,12 +5,14 @@ import TeamList from "@/components/TeamList";
 import { useEffect, useState } from "react";
 import { getTeams } from "@/app/actions";
 
+const delay:number = 2000
 export default function PageTeams() {
-    const [teams, setTeams] = useState<Team[]>([]);
+    const [teams, setTeams] = useState<Team[]>();
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         async function fetchTeams() {
+            console.log('call this teams')
             try {
                 const fetchedTeams = await getTeams();
                 setTeams(fetchedTeams);
@@ -19,8 +21,10 @@ export default function PageTeams() {
                 setError('Failed to fetch teams');
             }
         }
-        fetchTeams();
+        setTimeout(fetchTeams, delay);
     }, []);
+
+    if (!teams) return <div className="p-4">Loading... <span className="text-sm text-red-400">(with a fake delay of {delay} secs.)</span></div>;
 
     return (
         <div className="p-4">
