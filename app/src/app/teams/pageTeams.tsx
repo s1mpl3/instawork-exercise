437299b827020d2405f55d72f691965a3a1b40f9
s1mpl3 <jiriso@gmail.com>
2025-03-17 18:38:56 -0700

    Removing the simulated delay with the timeout. Adding the error message.

--- app/src/app/teams/pageTeams.tsx
+++ app/src/app/teams/pageTeams.tsx
@@ -5,26 +5,25 @@ import TeamList from "@/components/TeamList";
 import { useEffect, useState } from "react";
 import { getTeams } from "@/app/actions";
 
-const delay:number = 2000
 export default function PageTeams() {
     const [teams, setTeams] = useState<Team[]>();
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         async function fetchTeams() {
-            console.log('call this teams')
             try {
                 const fetchedTeams = await getTeams();
                 setTeams(fetchedTeams);
-                setError(null); // Clear any previous errors
+                setError(null);
             } catch {
                 setError('Failed to fetch teams');
             }
         }
-        setTimeout(fetchTeams, delay);
+        fetchTeams();
     }, []);
 
-    if (!teams) return <div className="p-4">Loading... <span className="text-sm text-red-400">(with a fake delay of {delay} secs.)</span></div>;
+    if (error) return <div className="p-4 text-red-500">{error}</div>;
+    if (!teams) return <div className="p-4">Loading...</div>;
 
     return (
         <div className="p-4">
